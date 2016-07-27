67d0116903cefe44c7376265c195b1af68cda333
emrickgarrett <emrickgj@miamioh.edu>
2016-07-26 23:49:27 -0600

    Weird bug when storing list in local storage... calling method 4 times it looks like.

--- scripts/easylistdownloader.js
+++ scripts/easylistdownloader.js
@@ -6,11 +6,11 @@ chrome.storage.local.get(["lastUpdate"], function(items){
     	return;
     }else{
     	lastUpdate = items.lastUpdate;
-    	alert(lastUpdate);
+    	//alert(lastUpdate);
     }
 
     //If it hasn't updated in an hour, redownload the file.
-	if(lastUpdate == null || lastUpdate === undefined || lastUpdate < Date.Now() - 1000*60*60*24){
+	if(lastUpdate == null || lastUpdate === undefined || lastUpdate < new Date().getTime() - 1000*60*60*24){
 		
 		var request = {type: "lastUpdate", data: lastUpdate + ""};
 		downloadData();
@@ -25,8 +25,47 @@ function downloadData(){
 		conflictAction: "overwrite"
 	});
 
-	chrome.storage.local.set({ "lastUpdate": "" + Date.Now() }, function(){
+	retrieveEasyList();
+
+
+	chrome.storage.local.set({ "lastUpdate": "" + new Date().getTime() }, function(){
 	    //  Data is saved, continue
 	});
 
+}
+
+function getEasyList(){
+	chrome.storage.local.get(["easylist"], function(items){
+		if(chrome.runtime.lastError){
+			//Error
+			alert("error");
+			return null;
+		}else{
+			alert("Item: " + items.easylist);
+			return items.easylist;
+		}
+	});
+}
+
+function storeEasyList(data){
+		chrome.storage.local.set({"easylist" : data}, function(){
+			if(chrome.runtime.lastError){
+				alert("error");
+			}
+		// Data saved!
+		console.log("Data_saved");
+		getEasyList();
+	})
+}
+
+
+function retrieveEasyList(){
+	var xmlhttp=new XMLHttpRequest();
+	xmlhttp.open("GET", "https://easylist.to/easylist/easylist.txt");
+	xmlhttp.onreadystatechange = function(){
+		var result = xmlhttp.responseText;
+		//console.log(result);
+		storeEasyList(result);
+	}
+	xmlhttp.send();
 }
